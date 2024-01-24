5d2f9c15f64b411a8a51922f3c04c55f7340a83b
amorales <alexmoraleschicaiza@gmail.com>
2024-01-24 17:03:31 +0100

    Fatta pagina d'introduzione

---
+++ app/src/main/java/com/example/progettoricettaesameu/activity_introduction.kt
@@ -0,0 +1,11 @@
+package com.example.progettoricettaesameu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_introduction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+    }
+}
